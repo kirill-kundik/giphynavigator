c39f009e3b886fcf740bea0d6c3764d79d890b93
kyrylo_kundik <goliathusua@gmail.com>
2021-11-03 17:23:14 +0200

    giphy services improvements;

--- giphynavigator/services/giphy_service.py
+++ giphynavigator/services/giphy_service.py
@@ -1,16 +1,31 @@
 """Giphy client module."""
 
-from aiohttp import ClientSession, ClientTimeout
+from aiohttp import ClientSession, ClientTimeout, ClientResponseError
+
+
+class GiphyServiceException(Exception):
+    pass
 
 
 class GiphyService:
     API_URL = "https://api.giphy.com/v1"
 
-    def __init__(self, api_key, timeout):
+    def __init__(self, api_key: str, timeout: float):
         self._api_key = api_key
         self._timeout = ClientTimeout(timeout)
 
-    async def search(self, query, limit=50, offset=0):
+    async def gif(self, id_: str) -> dict:
+        url = f"{self.API_URL}/gifs/{id_}"
+        params = {
+            "api_key": self._api_key,
+        }
+        try:
+            return await self._fetch(url, params)
+
+        except ClientResponseError as e:
+            raise GiphyServiceException(f"cannot load gif: {e.message}") from e
+
+    async def search(self, query: str, limit: int = 50, offset: int = 0) -> dict:
         """Make search API call and return result."""
         url = f"{self.API_URL}/gifs/search"
         params = {
@@ -19,6 +34,9 @@ class GiphyService:
             "limit": limit,
             "offset": offset,
         }
+        return await self._fetch(url, params)
+
+    async def _fetch(self, url: str, params: dict) -> dict:
         async with ClientSession(timeout=self._timeout) as session:
             async with session.get(url, params=params) as response:
                 if response.status != 200:
--- giphynavigator/services/search_service.py
+++ giphynavigator/services/search_service.py
@@ -1,25 +1,72 @@
 """Search service module."""
 
 import asyncio
+import logging
 from typing import Optional, List
 
 from giphynavigator.models import Gif, SearchObject
-from giphynavigator.services.giphy_service import GiphyService
+from giphynavigator.services.giphy_service import GiphyService, GiphyServiceException
 from giphynavigator.services.redis_service import RedisService
 
 
 class SearchService:
-
     def __init__(self, giphy_client: GiphyService, redis_service: RedisService):
         self._giphy_client = giphy_client
         self._redis_service = redis_service
+
+    @staticmethod
+    def _parse_gif(gif_object: dict) -> Gif:
+        gif = {
+            "id": gif_object["id"],
+            "url": gif_object["url"],
+            "embed_url": gif_object["embed_url"],
+        }
+        if "user" in gif_object:
+            gif["author_username"] = gif_object["user"]["username"]
+            gif["author_profile"] = gif_object["user"]["profile_url"]
+            gif["author_avatar"] = gif_object["user"]["avatar_url"]
+        return Gif(**gif)
+
+    async def _load(self, query: str, offset: int) -> List[str]:
+        results = await self._giphy_client.search(query, offset=offset)
+
+        ids = []
+
+        for original_gif in results["data"]:
+            gif = self._parse_gif(original_gif)
+            ids.append(gif.id)
+
+            await self._redis_service.set(f"gif_{gif.id}", gif.dict())
+
+        previous_results = await self._redis_service.get(f"search_{query}")
+
+        if previous_results:
+            ids_to_write = previous_results + ids
+        else:
+            ids_to_write = ids
+
+        await self._redis_service.set(f"search_{query}", ids_to_write)
+
+        return ids
 
     async def gif(self, id_: str) -> Optional[Gif]:
         gif = await self._redis_service.get(f"gif_{id_}")
 
         if gif:
             return Gif(**gif)
-        return None
+
+        try:
+            result = await self._giphy_client.gif(id_)
+
+        except GiphyServiceException as e:
+            logging.error(repr(e))
+            return None
+
+        gif = self._parse_gif(result["data"])
+
+        await self._redis_service.set(f"gif_{gif.id}", gif.dict())
+
+        return gif
 
     async def search(self, query: str, limit: int = 25, offset: int = 0) -> SearchObject:
         """Search for gifs and return formatted data."""
@@ -40,35 +87,3 @@ class SearchService:
                 for gif_id in results[offset:limit + offset]
             ]
         )
-
-    async def _load(self, query: str, offset: int) -> List[str]:
-        results = await self._giphy_client.search(query, offset=offset)
-
-        ids = []
-
-        for original_gif in results["data"]:
-            gif_id = original_gif["id"]
-            gif = {
-                "id": original_gif["id"],
-                "url": original_gif["url"],
-                "embed_url": original_gif["embed_url"],
-            }
-            if "user" in original_gif:
-                gif["author_username"] = original_gif["user"]["username"]
-                gif["author_profile"] = original_gif["user"]["profile_url"]
-                gif["author_avatar"] = original_gif["user"]["avatar_url"]
-
-            ids.append(gif_id)
-
-            await self._redis_service.set(f"gif_{gif_id}", gif)
-
-        previous_results = await self._redis_service.get(f"search_{query}")
-
-        if previous_results:
-            ids_to_write = previous_results + ids
-        else:
-            ids_to_write = ids
-
-        await self._redis_service.set(f"search_{query}", ids_to_write)
-
-        return ids
